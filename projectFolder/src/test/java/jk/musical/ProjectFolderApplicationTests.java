1dd419d08cfa77909eed77aa9b802b93c38e5840
Juyoung Kim <kjyoung153@hotmail.com>
2021-03-06 13:27:31 +0900

    Music_Market portfolio initiated

---
+++ projectFolder/src/test/java/jk/musical/ProjectFolderApplicationTests.java
@@ -0,0 +1,13 @@
+package jk.musical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFolderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
