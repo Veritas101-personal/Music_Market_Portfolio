2e96955be7e831063493713cfa0687f91bd18840
Juyoung Kim <kjyoung153@hotmail.com>
2021-03-08 00:16:08 +0900

    Project type changed from Spring Boot to Spring MVC

---
+++ src/main/java/jk/musicmarket/kr/vo/MemberVo.java
@@ -0,0 +1,80 @@
+package jk.musicmarket.kr.vo;
+
+import java.util.Date;
+
+public class MemberVo {
+
+	private int mb_num;
+	private String mb_id;
+	private String mb_password;
+	private String mb_name;
+	private String mb_nickname;
+	private Date mb_datetime;
+	private Date mb_disDate;
+	private String mb_cardNum;
+	private String mb_cardYear;
+	private String mb_cardMonth;
+	
+	public int getMb_num() {
+		return mb_num;
+	}
+	public void setMb_num(int mb_num) {
+		this.mb_num = mb_num;
+	}
+	public String getMb_id() {
+		return mb_id;
+	}
+	public void setMb_id(String mb_id) {
+		this.mb_id = mb_id;
+	}
+	public String getMb_password() {
+		return mb_password;
+	}
+	public void setMb_password(String mb_password) {
+		this.mb_password = mb_password;
+	}
+	public String getMb_name() {
+		return mb_name;
+	}
+	public void setMb_name(String mb_name) {
+		this.mb_name = mb_name;
+	}
+	public String getMb_nickname() {
+		return mb_nickname;
+	}
+	public void setMb_nickname(String mb_nickname) {
+		this.mb_nickname = mb_nickname;
+	}
+	public Date getMb_datetime() {
+		return mb_datetime;
+	}
+	public void setMb_datetime(Date mb_datetime) {
+		this.mb_datetime = mb_datetime;
+	}
+	public Date getMb_disDate() {
+		return mb_disDate;
+	}
+	public void setMb_disDate(Date mb_disDate) {
+		this.mb_disDate = mb_disDate;
+	}
+	public String getMb_cardNum() {
+		return mb_cardNum;
+	}
+	public void setMb_cardNum(String mb_cardNum) {
+		this.mb_cardNum = mb_cardNum;
+	}
+	public String getMb_cardYear() {
+		return mb_cardYear;
+	}
+	public void setMb_cardYear(String mb_cardYear) {
+		this.mb_cardYear = mb_cardYear;
+	}
+	public String getMb_cardMonth() {
+		return mb_cardMonth;
+	}
+	public void setMb_cardMonth(String mb_cardMonth) {
+		this.mb_cardMonth = mb_cardMonth;
+	}
+	
+	
+}
